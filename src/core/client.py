38e3b4dbd5b317f86bf108506609ce748918ae8f
Curtis Babnik <curtisbabnik@gmail.com>
2022-09-15 22:43:53 -0700

    Rename buckbot buckbotPy

--- src/core/client.py
+++ src/core/client.py
@@ -1,7 +1,7 @@
 import discord
 from discord import Client, Intents
 from auth import LOGIN_TOKEN
-from config import BOT_NAME
+from config import BOT_NAME, HIDDEN, GUILD_ID
 import logging
 
 class MyClient(Client):
@@ -16,4 +16,14 @@ class MyClient(Client):
 
     async def on_ready(self):
         print(f"Logged on as {self.user}")
-        await self.change_presence(status=discord.Status.dnd, activity=discord.Game("dead"))
+        if (HIDDEN):
+            await self.change_presence(status=discord.Status.invisible)
+        else:
+            await self.change_presence(status=discord.Status.dnd, activity=discord.Game("dead"))
+
+        if self.user.name != BOT_NAME:
+            await self.user.edit(username=BOT_NAME)
+        guild = self.get_guild(GUILD_ID)
+        member = guild.get_member(533749978955513856)
+        if member.nick != BOT_NAME:
+            await member.edit(nick=BOT_NAME)
